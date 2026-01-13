e8d85609eebaa046db4c9ef45ba8af21b0619a0f
Pizels29 <156490723+Pizels29@users.noreply.github.com>
2026-01-13 09:50:58 -0500

    Made it easier for users to read and add tasks

--- todoApp.py
+++ todoApp.py
@@ -1,17 +1,18 @@
 added_tasks = []
 def addtask():
-    a = input("Add task")
-    print("Task added:", a)
+    a = input("Add task: ")
+    print("Task added: ", a)
     added_tasks.append(a)
-    print("All tasks:", added_tasks)
+    print("All tasks: ", added_tasks)
     print(repeat())
 def repeat():
-    b = input("Press 1 to add another task or 2 to complete your task")
+    b = input("Press 1 to add another task or 2 to complete your task: ")
     print(b)
     if b == "1":
         addtask()
     elif b == "2":
-        a = input("Enter the task you have completed")
+        a = input("Enter the task you have completed: ")
         added_tasks.remove(a)
-        print("Your remaining tasks are", added_tasks)
+        print("Your remaining tasks are: ", added_tasks)
 print(addtask())
+#Made by Aarnav Dave during period 3 band when I was wasting time lol
